cedd0b14f43fec51ed08639be818831df1208e6b
junghyun326 <jonghyun4488@gmail.com>
2024-08-14 16:07:58 +0900

    update

--- producers/src/main/java/com/example/kafka/SimpleProducer.java
+++ producers/src/main/java/com/example/kafka/SimpleProducer.java
@@ -8,34 +8,39 @@ import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 
+import static java.lang.Thread.sleep;
+
 public class SimpleProducer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         String topicName = "test-topic";
 
         // kafka producer configuration
-
-        Properties props = new Properties();
-        // bootstrap.servers, key.serializer.class, value.serializer.class
+        for(int i = 0; i < 1; i++){
+            System.out.println(i + "번째 동작입니다.");
+            Properties props = new Properties();
+            // bootstrap.servers, key.serializer.class, value.serializer.class
 //        props.setProperty("bootstrap.servers", "52.78.108.58:9092");
-        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "52.78.108.58:9092");
-        // both are correct, but lower code is more stable when the parameter's name change
-        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+            props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "43.201.49.249:31187");
+            // both are correct, but lower code is more stable when the parameter's name change
+            props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+            props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
-        // KafkaProducer Object
-        // <key object type, value object type>
-        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<String, String>(props);
+            // KafkaProducer Object
+            // <key object type, value object type>
+            KafkaProducer<String, String> kafkaProducer = new KafkaProducer<String, String>(props);
 
-        // ProducerRecord Object
-        // if key parameter is empty, key will be null
-        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topicName, "hello world2");
+            // ProducerRecord Object
+            // if key parameter is empty, key will be null
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topicName, "hello world" + i);
 
-        // KafkaProducer message send Object
-        kafkaProducer.send(producerRecord);
+            // KafkaProducer message send Object
+            kafkaProducer.send(producerRecord);
 
-        // flush : to send message in the buffer
-        kafkaProducer.flush();
-        kafkaProducer.close();
+            // flush : to send message in the buffer
+            kafkaProducer.flush();
+            kafkaProducer.close();
+            sleep(1000);
+        }
     }
 }
